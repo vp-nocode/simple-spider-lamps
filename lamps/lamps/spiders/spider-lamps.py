2cba1d0cadefd66519df79ed903abd05176f8b40
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-06-22 18:49:03 +0500

    added spider project

---
+++ lamps/lamps/spiders/spider-lamps.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class SpiderLampsSpider(scrapy.Spider):
+    name = "spider-lamps"
+    allowed_domains = ["atlantsnab.ru"]
+    start_urls = ["https://atlantsnab.ru/catalog/istochniki-sveta/"]
+
+    def parse(self, response):
+        lamps = response.css('div.product-text-wrap.d-flex.flex-column')
+        for lamp in lamps:
+            yield {
+                'name' : lamp.css('a.description.w-100.itempropname').attrib['title'],
+                # 'name' : lamp.css('a.description.w-100.itempropname::text').get().strip()
+                'price' : lamp.css('div.pt-3.w-100 span::text').get().strip(),
+                'article' : lamp.css('a.sku::text').get().strip(),
+                'url' : lamp.css('a.description.w-100.itempropname').attrib['href']
+            }
